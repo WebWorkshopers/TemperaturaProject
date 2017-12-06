30fc374569aadcd353e2eeacc92abf273e9679fc
Cristóbal Ganter <cganterh@gmail.com>
2017-12-05 22:31:49 -0300

    Added temproject.get_app() and temproject.run()

---
+++ temproject/__init__.py
@@ -0,0 +1,25 @@
+"""A temperature satisfaction logging app for offices."""
+
+import signal as _signal
+
+from tornado.ioloop import IOLoop as _IOLoop
+from tornado.log import enable_pretty_logging as _enable_pretty_logging
+from tornado.web import Application as _Application
+
+
+def get_app():
+    """Return a new temproyect application."""
+    return _Application(debug=True)
+
+
+def run():
+    """Run the temproject application."""
+    _signal.signal(
+        _signal.SIGINT,
+        lambda _, __: _IOLoop.current().stop()
+    )
+
+    get_app().listen(8888)
+    _enable_pretty_logging()
+    _IOLoop.current().start()
+
